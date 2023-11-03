aef374f79b12a92438864318be52b75416acf308
Boussif Isai <eexdhe@gmail.com>
2023-11-03 15:36:49 +0100

    feat: add todo's id

--- database/todo-repository.go
+++ database/todo-repository.go
@@ -1,6 +1,8 @@
 package database
 
-import "github.com/eexy/gotos/model"
+import (
+	"github.com/eexy/gotos/model"
+)
 
 type TodoRepository struct {
 	Database *Database
@@ -8,10 +10,25 @@ type TodoRepository struct {
 
 func (r *TodoRepository) Add(title string) *model.Todo {
 	todos := r.Database.LoadDb()
-	todo := &model.Todo{Title: title, Completed: false}
+	todo := &model.Todo{Id: r.generateNewTodoId(todos), Title: title, Completed: false}
 	todos = append(todos, todo)
 	r.Database.SaveDb(todos)
 	return todo
+}
+
+func (r *TodoRepository) generateNewTodoId(todos []*model.Todo) int {
+	if len(todos) == 0 {
+		return 0
+	}
+
+	max := todos[0].Id
+	for _, todo := range todos {
+		if todo.Id > max {
+			max = todo.Id
+		}
+	}
+
+	return max + 1
 }
 
 func (r *TodoRepository) Get() []*model.Todo {
